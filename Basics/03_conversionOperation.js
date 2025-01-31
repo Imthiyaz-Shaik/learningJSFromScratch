82e9244f8d65d1ddfebca603412364ff9381fc86
Imthiyaz-Shaik <20pp5a0275@gmail.com>
2025-01-31 23:26:04 +0530

    added conversion of dataTypes

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score='70abc'
+
+console.log(typeof score)
+console.log(typeof(score))   //can also write in this manner
+
+let valueInNumbers=Number(score)
+console.log(typeof valueInNumbers)
+console.log(valueInNumbers)  //NaN
+
+// '33'=> 33
+//'3abc' => NaN
+//true => 1; false =>0
+
+let name =0
+
+let changedName=Boolean(name)
+console.log(changedName)
+
+//1 => true; 0 => false
+//'imthiyaz' => true
+// '' => false
