231d75591d508436ea734a73639a55ae6c17a25e
Imthiyaz-Shaik <20pp5a0275@gmail.com>
2025-01-31 22:54:28 +0530

    added  datatypes.js

---
+++ Basics/dataTypes.js
@@ -0,0 +1,9 @@
+'use strict'; //treat all JS code as newer version
+
+// alert(3+3)  alert has differnt syntax in Node.js
+
+console.log('3'+'3')
+
+console.log(typeof undefined)  //undefined
+
+console.log(typeof null)  //object
---
+++ Basics/variables.js
@@ -0,0 +1,13 @@
+const accountId=122357;
+let accountEmail='imthiyaz123@gmail.com'
+var accountPassword='12344567'
+accountCity='hyderabad'
+
+// accountId=2
+// not allowed to do so cuz const can't be changed
+
+ accountEmail='jhdchcfbh@53623.com'
+ accountPassword='728363277'
+ accountCity='bengaluru'
+
+ console.table([accountId,accountEmail,accountPassword,accountCity])
